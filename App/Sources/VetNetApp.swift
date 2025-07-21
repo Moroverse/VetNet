2f349357a87748a140c9150099dbea1df90c8754
Daniel Moro <daniel.moro@me.com>
2025-07-21 22:45:47 +0200

    reset

--- App/Sources/VetNetApp.swift
+++ App/Sources/VetNetApp.swift
@@ -1,34 +1,10 @@
 import SwiftUI
-import SwiftData
-import Factory
 
 @main
 struct VetNetApp: App {
-    
-    // SwiftData model container with CloudKit synchronization
-    let modelContainer: ModelContainer
-    
-    init() {
-        do {
-            self.modelContainer = try VeterinaryDataStore.createModelContainer()
-        } catch {
-            fatalError("Failed to initialize model container: \(error)")
-        }
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(modelContainer)
-                .onAppear {
-                    // Initialize any app-level setup here
-                    setupVeterinaryServices()
-                }
         }
-    }
-    
-    private func setupVeterinaryServices() {
-        // Services are automatically registered through Factory Container extensions
-        // This method can be used for any additional initialization
     }
 }
